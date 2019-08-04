aa65199ac30e6400300b22288f97aa42a84baa04
Julia Zajusz <julia.zajusz@fingo.pl>
2019-08-04 17:17:58 +0200

    kotlin graphql project initialization

---
+++ house-med-assistant-app/src/main/kotlin/com/shacky/housemedassistant/HouseMedAssistantApplication.kt
@@ -0,0 +1,11 @@
+package com.shacky.housemedassistant
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HouseMedAssistantApplication
+
+fun main(args: Array<String>) {
+	runApplication<HouseMedAssistantApplication>(*args)
+}
