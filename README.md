f480e93810670a84e0efd25f9e08299fc8fdf6e2
Julia Zajusz <julia.zajusz@fingo.pl>
2019-08-25 10:11:28 +0200

    first working test

--- README.md
+++ README.md
@@ -1,130 +1,17 @@
-# mutation {
-#   newSnack(name: "French Fries", amount: 40.5) {
-#     id
-#     name
-#     amount
-#   }
-# }
+#Algorytmy genetyczne do zarządzania flotą pojazdów w systemie domowej opieki medycznej 
+ ##Genetic algorithms for fleet vehicles management in home medical care system
+
+#Requirements
+mongo datatbase
+
+#Run application 
+gradlew house-med-assistant:bootRun
+
+http://localhost:9000/graphiql
 
 
-
-# mutation {
-#     newReview(snackId:"14d029c6-6fdb-4137-8682-c44faf4af853",
-#     text: "Awesome snack!", rating:5
-#     ){
-#         snackId, text, rating
-#     }
-# }
-
-
-# query {
-#   snacks {
-#     name,
-#     reviews {
-#       text, rating
-#     }
-#   }
-# }
-
-# wyciąganie wszystkich punktow z ich lokalizacjami z bazy
-query punkty{
-  points {
-    id,
-    name,
-    coordinate {
-      location
-    }
-  }
-}
-
-#wyciaganie wszystkich koordynatow
-query koordynaty{
-  coordinates {
-    id,
-    location
-  }
-}
-
-
-#wyciaganie wszystkich scieżek
-query sciezki{
-	paths {
-  points {
-    id,
-    location
-  }
-  }
-}
-
-#tworzenie nowego koordynatu
-mutation nowyKoordynat {
-    newCoordinate(
-    	location: [2.0, 2.0]
-    ){
-        id, location
-    }
-}
-
-
-
-#tworzenie nowego punktu
-mutation nowyPunkt{
-  newPoint(name: "Wieża Eifflea", location: [4.0, 5.6]) {
-    id
-    name
-    coordinate {
-      id,
-      location
-    }
-  }
-}
-
-
-
-
-#tworzenie nowej scieżki
-mutation nowaSciezka{
-  newPath(points: [      {
-        id: "d3535453-1975-4b4e-a42e-30aaf976dac1",
-        location: [
-          2,
-          2
-        ]
-      },
-      {
-        id: "00abfc6f-76d5-4509-9705-89bdbcf018eb",
-        location: [
-          4,
-          5.6
-        ]
-      }]) {
-  points {
-    id,
-    location
-  }
-  }
-}
-
-#tworzenie nowej scieżki bez wczesniej utworzonych punktow
-mutation nowaSciezka2{
-  newPath(points: [      {
-        location: [
-          2.3,
-          2.4
-        ]
-      },
-      {
-        location: [
-          44,
-          55.6
-        ]
-      }]) {
-  points {
-    id,
-    location
-  }
-  }
-}
-
-
-
+#Stack
+Kotlin
+GraphQL
+MongoDB
+React
---
+++ house-med-assistant-app/src/test/kotlin/com/shacky/housemedassistant/GeospacialLocalizationTest.kt
@@ -0,0 +1,83 @@
+package com.shacky.housemedassistant
+
+//import com.mongodb.client.model.geojson.Point
+import com.mongodb.MongoClient
+import com.mongodb.client.MongoCollection
+import com.mongodb.client.MongoDatabase
+import com.mongodb.client.model.Filters
+import com.shacky.housemedassistant.entity.Coordinate
+import com.shacky.housemedassistant.entity.Point
+import com.shacky.housemedassistant.repository.PointRepository
+import com.shacky.housemedassistant.resolvers.PointMutationResolver
+import com.shacky.housemedassistant.resolvers.PointQueryResolver
+import org.bson.Document
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+
+@RunWith(SpringRunner::class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class GeospatialLiveTest {
+//class GeospatialLiveTest(
+//        @Autowired val coordinateMutationResolver: CoordinateMutationResolver,
+//        @Autowired val coordinateQueryResolver: CoordinateQueryResolver,
+//        @Autowired val coordinateRepository: CoordinateRepository
+//) {
+
+    private var mongoClient: MongoClient? = null
+    private var db: MongoDatabase? = null
+    private var collection: MongoCollection<Document>? = null
+
+    @Autowired
+    lateinit var pointMutationResolver: PointMutationResolver
+
+    @Autowired
+    lateinit var pointQueryResolver: PointQueryResolver
+
+    @Autowired
+    lateinit var pointRepository: PointRepository
+
+    @Before
+    fun setup() {
+        if (mongoClient == null) {
+            mongoClient = MongoClient()
+            db = mongoClient!!.getDatabase("kotlin-graphql")
+            collection = db!!.getCollection("point")
+        }
+    }
+
+
+    @Test
+    fun getInsertedObjects() {
+//        val result = coordinateRepository.findAll();
+//        val result = coordinateQueryResolver.coordinates();
+//        println(result[0])
+    }
+
+    @Test
+    fun givenNearbyLocation_whenSearchNearby_thenFound() {
+//        val currentLoc = Point(Position(-0.126821, 51.495885))
+//        val result = collection!!.find(Filters.near("location", currentLoc, 1000.0, 10.0))
+//
+//        assertNotNull(result.first())
+//        assertEquals("Big Ben", result.first()!!["name"])
+    }
+
+    @Test
+    fun givenNearbyLocation_whenSearchWithinCircleSphere_thenFound() {
+//            val a = pointMutationResolver.newPoint("Westminster, Londyn ", listOf(-0.1435083f, 51.4990956f))
+        val a = Point("Westminster, Londyn ", Coordinate(listOf(-0.1435083f, 51.4990956f)))
+        val distanceInRad = 5.0 / 6371
+        val result = collection!!.find(Filters.geoWithinCenterSphere("coordinate.location", a.coordinate.location[0].toDouble(), a.coordinate.location[1].toDouble(), distanceInRad))
+        assertNotNull(result.first())
+        assertEquals("Big Ben", result.first()!!["name"])
+    }
+
+
+}
